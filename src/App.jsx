9afa4332166d4fcbc1a4a51a118738718f9c9227
Lina  Melendez <melendezvideslinadelcamen@gmail.com>
2025-10-31 17:57:44 -0500

    Commit  frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,42 @@
-function App(){
-  const nombre= "ajkaa"
- return(
-  <div>
-  <header>Hola{nombre}</header>
-  <footer>Adios</footer>
-  </div>
 
- )
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [juegos, setJuegos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const obtenerJuegos = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/juegos');
+        setJuegos(response.data.juegos);
+      } catch (error) {
+        console.error('Error:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    obtenerJuegos();
+  }, []);
+
+  if (loading) return <p>Cargando...</p>;
+
+  return (
+    <div>
+      <header>
+        <h1>GamerTracker</h1>
+      </header>
+      <main>
+        {juegos.map(juego => (
+          <div key={juego.id}>
+            <h2>{juego.nombre}</h2>
+            <p>Año: {juego.año}</p>
+          </div>
+        ))}
+      </main>
+    </div>
+  );
 }
-export default App
+
+export default App;
